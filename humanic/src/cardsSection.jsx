c15d21a71de2935be04d79c1b2e55cc7f8fb0f20
KushajM <kushaj.fsociety@gmail.com>
2025-04-30 23:27:36 +0530

    size fix

--- humanic/src/cardsSection.jsx
+++ humanic/src/cardsSection.jsx
@@ -33,7 +33,7 @@ export default function CardsSection() {
     <img
       src="/Group 128.png"
       alt="Mobile Placeholder"
-      className="w-[100%] h-auto object-contain"
+      className="w-[100%] h-[100%] object-contain"
     />
   </div>
       
