916bf7e273a24f48581cfb9e81e87d91b853925d
KushajM <kushaj.fsociety@gmail.com>
2025-05-01 01:52:09 +0530

    navbar gap 1

--- humanic/src/App.jsx
+++ humanic/src/App.jsx
@@ -74,7 +74,7 @@ useEffect(() => {
   </NavLink>
 
    {/* Spacer between Doctrine and Book a demo */}
-   <div className="hidden lg:block" style={{ width: '4.058vw' }}></div>
+   <div className="hidden lg:block" style={{ width: '1.058vw' }}></div>
   <div className="lg:hidden" style={{ width: '0vw' }}></div>
 
   {/* Book a demo - only visible on md+ */}
